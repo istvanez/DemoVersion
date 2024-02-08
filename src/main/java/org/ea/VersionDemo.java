abc9707e4eb50623bf8ba13717206f47ddb7eac2
Elias Zenteno Istvan Alan <ieliasz@comercio.local>
2024-02-07 19:27:41 -0500

    Commit during write example

--- src/main/java/org/ea/VersionDemo.java
+++ src/main/java/org/ea/VersionDemo.java
@@ -4,7 +4,7 @@ public class VersionDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo equipo elias");
 		System.out.println();
 		System.out.println("Este programa en ejecucion es la version:"
 				+ VersionDemo.class.getPackage().getImplementationVersion());
