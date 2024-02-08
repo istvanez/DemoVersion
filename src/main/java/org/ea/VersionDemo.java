d20128d621de31860db843c132b9bbf51e77b099
Elias Zenteno Istvan Alan <ieliasz@comercio.local>
2024-02-07 20:09:12 -0500

    preparando los cambios para el SR-XXX5.0.1

--- src/main/java/org/ea/VersionDemo.java
+++ src/main/java/org/ea/VersionDemo.java
@@ -5,6 +5,7 @@ public class VersionDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("********* Hola mundo equipo elias ortiz  AKT *******");
+		System.out.println("********* ================================== *******");
 		System.out.println();
 		System.out.println("Este programa en ejecucion es la version:"
 				+ VersionDemo.class.getPackage().getImplementationVersion());
